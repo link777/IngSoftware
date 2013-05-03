3bd5e786dc14164d03faa448a42a2aa33082aedb
Patricio Espinoza Salgado <the_stone1@hotmail.com>
2013-05-03 16:58:34 -0400

    Update

--- Tarea 3/main.cpp
+++ Tarea 3/main.cpp
@@ -1,35 +1,34 @@
 
     /*-----------------------------------------------*
-     |     Ingenieria en Software:                   |
-     |                                               |
-     |     Integrante: Patricio Espinoza Salgado     |
-     |                                               |
-     *-----------------------------------------------*/
+| Ingenieria en Software: |
+| |
+| Integrante: Patricio Espinoza Salgado |
+| |
+*-----------------------------------------------*/
 
 #include <iostream>
 #include <libpq-fe.h>
 
 using namespace std;
 
+PGconn *cnn = NULL;
+PGresult *result = NULL;
+
+char *host = "sebastian.cl";
+char *port = "5432";
+char *dataBase = "iswdb";
+char *user = "isw";
+char *passwd = "isw";
 
 int main(int argc, char * argv[])
 {
-    PGconn *cnn = NULL;
-    PGresult *result = NULL;
-
-    char *host = "sebastian.cl";
-    char *port = "5432";
-    char *dataBase = "iswdb";
-    char *user = "isw";
-    char *passwd = "isw";
-
     int i;
 
    cnn = PQsetdbLogin(host,port,NULL,NULL,dataBase,user,passwd);
 
     if (PQstatus(cnn) != CONNECTION_BAD) {
         cout << "\t\t--------------------------------------" << endl;
-        cout << "\t\t|  Conexion al Servidor Establecida  |" << endl;
+        cout << "\t\t| Conexion al Servidor Establecida |" << endl;
         cout << "\t\t--------------------------------------" << endl << endl;
 
         result = PQexec(cnn, "SELECT C.docente_id as \"ID Profesor\", AVG(A.nota) as \"Promedio Notas\", STDDEV(A.nota) as \"Desviacion Estandar\" FROM cursos C INNER JOIN asignaturas_cursadas A ON A.curso_id = C.curso_id GROUP BY C.docente_id");
@@ -45,7 +44,7 @@ int main(int argc, char * argv[])
             cout << "Los nombres de las columnas son:" << endl << endl;
 
             for (i=0; i<columnas; i++) {
-                cout << PQfname(result,i) << "    |    ";
+                cout << PQfname(result,i) << " | ";
             }
 
             cout << endl;
@@ -54,7 +53,7 @@ int main(int argc, char * argv[])
 
             for (i=0; i<filas; i++) {
                 for (int j=0; j<columnas; j++) {
-                    cout  << PQgetvalue(result,i,j) << "           ";
+                    cout << PQgetvalue(result,i,j) << " ";
                 }
                 cout << endl;
             }
