46b195e61c157231648b43290330a110efd04d8a
Patricio Espinoza Salgado <the_stone1@hotmail.com>
2013-05-26 05:51:03 -0400

    Update estetico Autenticar.php

--- Tarea 8/autenticar.php
+++ Tarea 8/autenticar.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/estilo.css" />
+<title>Autenticar</title>
 <?php 
 	header("Content-type: text/html; charset=utf8");
 	session_start();
@@ -35,7 +43,7 @@
         }
         else
 	        {
-			exit ("<txt>Rut o contraseña incorrecta</txt> <a href='javascript:history.back(-1)'>Volver</a>");
+			exit ("<txt>Rut o contraseña incorrecta.</txt> <a href='javascript:history.back(-1)'>Volver</a>");
 	        }
 	        
 
@@ -70,3 +78,10 @@
 echo '<txt>Debug</txt><pre>' . htmlspecialchars($cliente->debug_str, ENT_QUOTES) . '</pre>';
 */
 ?>
+
+</head>
+
+<body>
+</body>
+
+</html> 
